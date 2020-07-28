b411a23c977e206bccae8ee2a91f22519019bc03
Mariany Ferreira <myf@certi.org.br>
2020-07-28 13:17:03 -0300

    feat: create Microfrontend APP2 using single-spa

---
+++ app2/src/myf-app2.js
@@ -0,0 +1,28 @@
+import "./set-public-path";
+import React from "react";
+import ReactDOM from "react-dom";
+import singleSpaReact from "single-spa-react";
+import Root from "./root.component";
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Root,
+  domElementGetter,
+  errorBoundary(err, info, props) {
+    // Customize the root error boundary for your microfrontend here.
+    return null;
+  },
+});
+
+function domElementGetter() {
+  let element = document.getElementById("microfrontend-apps");
+  if (!element) {
+    element = document.createElement("div");
+    element.id = "myf-app2";
+    document.body.appendChild(element);
+  }
+  return element;
+}
+
+export const { bootstrap, mount, unmount } = lifecycles;
